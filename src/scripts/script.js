81bb8812d58ee6e7b1e90a0f458503884d13db16
Dariush vesal <dariush.vesal@gmail.com>
2018-10-17 11:10:16 +0330

    Add starter text

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,6 @@
 const ES6Test = (...args) => {
   console.log(args.join(' '));
+  document.getElementById('description').innerHTML = `With Boilerplate Gulp Sass Babel`;
 }
 
 ES6Test(`Hello`, `wordl`, `!`);
